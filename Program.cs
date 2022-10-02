36eec36377adcbe9a36916b257bee2074f224456
Marvish Subhas Chandra <mschandr@uci.edu>
2022-10-01 17:59:55 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 // Marvish Chandra
 
 namespace menJeans{
-    class Straight{}
+    class typeStraight{
+        premium501 = int;
+        premium505 = int;
+        premium514 = int;
+        premium516 = int;
+        premium551 = int;
+        silvertab = int;
+        year50s = int;
+
+    }
+    class Straight{
+        premium501.black = 109.95;
+        premium501.
+
+    }
     class Slim{}
     class Straight{}
     class Tapered{}
